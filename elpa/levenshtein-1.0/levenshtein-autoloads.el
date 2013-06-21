fc022dd47e43695cb2e6be21a7021ad50788832b
lichenhao <lch14forever@gmail.com>
2013-06-21 23:15:31 +0800

    my first commit. Push the current settings to github

---
+++ elpa/levenshtein-1.0/levenshtein-autoloads.el
@@ -0,0 +1,18 @@
+;;; levenshtein-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("levenshtein-pkg.el" "levenshtein.el")
+;;;;;;  (20927 56077 950946 186000))
+
+;;;***
+
+(provide 'levenshtein-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; levenshtein-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,57 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;package repo
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;full screen
+;最大化
+(defun my-maximized ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+)
+;启动时最大化
+(my-maximized)
+;;line number
+(global-linum-mode 1)
+
+;;ESS config
+ (setq ess-eval-visibly-p nil) ;otherwise C-c C-r (eval region) takes forever
+ (setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
+;;cperl
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+;;Syntax Highlighting
+(if (fboundp 'global-font-lock-mode)
+    (global-font-lock-mode 1) ; GNU Emacs
+  (setq font-lock-auto-fontify t)) ; XEmacs
+(setq cperl-invalid-face nil) ;turn off "_"
+(setq cperl-electric-keywords t) ;; expands for keywords such as
+                                     ;; foreach, while, etc...
+;;flymake
+(require 'flymake)
+(global-set-key [f3] 'flymake-mode)
+(global-set-key [f4] 'flymake-display-err-menu-for-current-line)
+(global-set-key [f5] 'flymake-goto-next-error)
+;;(add-hook 'find-file-hook 'flymake-find-file-hook) ;;auto-start
