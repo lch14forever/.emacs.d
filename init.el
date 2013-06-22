fbaef5e97b39a230a002ae11e9af16476c576cba
lichenhao <lch14forever@gmail.com>
2013-06-22 15:51:28 +0800

    org mode setup

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;;Org mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
