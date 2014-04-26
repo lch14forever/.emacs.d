32c49184ac592d4d58da8b5b083b1741aefe7e6d
lichenhao <lch14forever@gmail.com>
2014-04-26 19:15:12 +0800

    updated ess mode

--- init.el
+++ init.el
@@ -107,6 +107,7 @@
 
 ;;********************R*********************
 ;;ESS config
+ (require 'ess-site)
  (setq ess-eval-visibly-p nil) ;otherwise C-c C-r (eval region) takes forever
  (setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
 ;;*******************Perl*******************
