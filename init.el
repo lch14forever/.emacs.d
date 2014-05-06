553909cee39dd2817e9eb71c10a7058d9d6ad4bf
lichenhao <lch14forever@gmail.com>
2014-05-07 02:49:06 +0800

    ess-doc

--- init.el
+++ init.el
@@ -108,6 +108,7 @@
 ;;********************R*********************
 ;;ESS config
 (require 'ess-site)
+(require 'ess-eldoc)
 (setq ess-eval-visibly-p nil) ;otherwise C-c C-r (eval region) takes forever
 (setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
 ;;*******************Perl*******************
