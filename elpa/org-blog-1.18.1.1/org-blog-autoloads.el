e672e103b9303c99e4c5cea25be73bcd0f782017
Li Chenhao <lch14forever@gmail.com>
2014-11-05 10:27:40 +0800

    upgraded org mode

---
+++ elpa/org-blog-1.18.1.1/org-blog-autoloads.el
@@ -0,0 +1,30 @@
+;;; org-blog-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (org-blog-new-post) "org-blog" "org-blog.el" (21593
+;;;;;;  34828 665277 475000))
+;;; Generated autoloads from org-blog.el
+
+(autoload 'org-blog-new-post "org-blog" "\
+Create a new post in FILENAME.
+Post is stored in `org-blog-unfinished-directory'.
+
+\(fn FILENAME)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("org-blog-pkg.el") (21593 34828 705277
+;;;;;;  75000))
+
+;;;***
+
+(provide 'org-blog-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; org-blog-autoloads.el ends here
